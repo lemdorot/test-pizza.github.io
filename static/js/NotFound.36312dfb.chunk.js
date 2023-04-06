3fa8ffcedaab3cd98851e5c2cdc5e694eb81e255
lemdorot <36665180+lemdorot@users.noreply.github.com>
2023-04-06 10:32:26 +0300

    Add files via upload

---
+++ static/js/NotFound.36312dfb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_pizza=self.webpackChunkreact_pizza||[]).push([[187],{4949:function(e,a,r){r.r(a);r(2791);var t=r(6159),u=r(184);a.default=function(){return(0,u.jsx)(t.vr,{})}}}]);
+//# sourceMappingURL=NotFound.36312dfb.chunk.js.map
